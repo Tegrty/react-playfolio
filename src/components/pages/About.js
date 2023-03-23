0ae994ea9b6e53097565c308e6796753d65f7ee6
Mario Repas <mario.repas63@gmail.com>
2023-03-22 21:56:22 -0400

    Feature: Added about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import logo from '../../assets/img/Mariorepas-01-01.png'
+import logo from '../../assets/img/Mariorepas-01-01.png';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
-export default function About() {
+const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-      {/* add image */}
-      <img src={logo} alt="mario-icon" />
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
+    <Container>
+      <Card className="about-card border-0 rounded-0 bg-light">
+        <Row className="g-0">
+          <Col lg={6} className="d-flex justify-content-center align-items-center">
+            <div className="about-img-container">
+              <Card.Img src={logo} alt="mario-icon" className="about-img img-fluid" />
+            </div>
+          </Col>
+          <Col lg={6}>
+            <Card.Body className="about-text">
+              <Card.Title as="h1" className="about-heading">About Page</Card.Title>
+              <Card.Text as="p" className="about-description">
+                Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+                Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+                mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+                lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+                imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+                in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+              </Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </Container>
   );
-}
+};
+
+export default About;
