687f264bcfe96249a8fb562941369768534c8b6a
Mario Repas <mario.repas63@gmail.com>
2023-03-22 22:11:53 -0400

    Feature: Added resume page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,32 @@
 import React from 'react';
 
-export default function Home() {
+const Resume = () => {
+  const downloadDocument = () => {
+    window.location.href = 'https://docs.google.com/document/d/1b6U17wSDXXEBFp4xWJBAmE25XBhSjuXBSgLoaZLxruY/edit?usp=sharing';
+  };
+
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <div className="resume-container">
+      <h1 className="resume-heading text-light text-center">Resume</h1>
+      <h2 className="resume-subheading text-light text-center">Click the button below to download my FULL resume.</h2>
+      <div className="resume-download">
+        <button className="btn btn-primary" onClick={downloadDocument}>
+          Download
+        </button>
+      </div>
+      <div className="resume-document">
+        <iframe 
+          title="resume-document"
+          src="https://docs.google.com/document/d/e/2PACX-1vQ6G24ac6R6kZ9ARhc9l_LDrmsWI4sZLphYjn0nKQlKKWHpvPuObqYrDAOpApRGXVnNJJoz8Vir21I3/pub?embedded=true"
+          width="100%"
+          height="600"
+          frameBorder="0"
+          scrolling="no"
+          style={{ overflow: 'auto' }}
+        ></iframe>
+      </div>
     </div>
   );
-}
+};
+
+export default Resume;
